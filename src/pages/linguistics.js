657292280a4a3668ade4ef5fb944390fa65aca37
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2021-09-03 21:15:26 +0900

    20210903 updated

--- src/pages/linguistics.js
+++ src/pages/linguistics.js
@@ -49,6 +49,12 @@ const LinguisticsPage = () => {
 </p>
 
 <h4>言語学の入門書</h4>
+<Link to="#general">言語学一般</Link>
+<br />
+<Link to="#phonetics">音声学</Link>
+<br />
+<Link to="comparative_linguistics">比較言語学</Link>
+
 <h5 id="general">言語学一般</h5>
 
 	<div
@@ -98,7 +104,6 @@ const LinguisticsPage = () => {
 </p>
 <hr />
 <h5 id="phonetics">音声学</h5>
-<hr />
 <p><Link to="https://amzn.to/3iL8MBx">小泉保（2003）『音声学入門』大学書林</Link></p>
 
 <div
@@ -137,6 +142,97 @@ const LinguisticsPage = () => {
 
 <p>
   いずれも古典的名著ですが、小泉のほうは入門であるのに対して服部のほうはしっかりした音声学の書です。昔の学生は服部四郎の音声学についていた音源レコードを聴いて勉強したといいます。服部四郎の門下生は口に鉛筆を入れられて調音点を直されたとか。
+</p>
+
+<hr />
+<h5 id="comparative_linguistics">比較言語学</h5>
+<p><Link to="https://amzn.to/3BALRj5">高津春繁（1992）『比較言語学入門』岩波文庫</Link></p>
+
+<div
+         style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+        }}>
+
+
+<Link to="https://www.amazon.co.jp/gp/product/4003367618/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4003367618&linkCode=as2&tag=dai5986-22&linkId=5bd1f95645100e1139e2aa01609a696b"><img border="0" src="//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4003367618&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=dai5986-22" /></Link>
+<p><Link to="https://amzn.to/3BALRj5">比較言語学入門</Link></p>
+
+        </div>
+<p>
+  古典ギリシャ語が専門だった著者による比較言語学入門書。古典ですが名著です。
+</p>
+
+<hr />
+<p><Link to="https://amzn.to/3DHUx9d">吉田和彦（1996）『言葉を復元する―比較言語学の世界』三省堂</Link></p>
+<div
+         style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+        }}>
+
+
+<Link to="https://www.amazon.co.jp/gp/product/4385357145/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4385357145&linkCode=as2&tag=dai5986-22&linkId=0451510508d0172ee8b5827d4405e005"><img border="0" src="//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4385357145&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=dai5986-22" /></Link>
+<p><Link to="https://amzn.to/3DHUx9d">言葉を復元する―比較言語学の世界</Link></p>
+        </div>
+
+
+<p>
+  古代ヒッタイト語を専門としていた著者による比較言語学の入門書。おおよその大事なことは書いてあります。言葉を復元する面白さが伝わる、良い本です。
+</p>
+
+<hr />
+
+<p><Link to="https://amzn.to/38FOeVb">黒田龍之介（2011）『ことばは変わる ─ はじめての比較言語学』白水社</Link></p>
+<div
+         style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+        }}>
+
+
+<Link to="https://www.amazon.co.jp/gp/product/4560085811/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4560085811&linkCode=as2&tag=dai5986-22&linkId=5ae915594e5c139b994a8252c6a9d48f"><img border="0" src="//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4560085811&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=dai5986-22" /></Link>
+<p><Link to="https://amzn.to/38FOeVb">ことばは変わる ─ はじめての比較言語学</Link></p>
+        </div>
+
+
+<p>
+  スラブ言語学を専門とする著者による比較言語学の入門書です。スラブ語の古典語といえば古教会スラブ語ですね。
+</p>
+
+<hr />
+
+<p><Link to="https://www.amazon.co.jp/gp/product/B000J8V7WS/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B000J8V7WS&linkCode=as2&tag=dai5986-22&linkId=b883c931bcf6e376d9236ef3c46328bc">アントワヌ・メイエ、（1977）『史的言語学における比較の方法』みすず書房</Link></p>
+<div
+         style={{
+          // By using the same grid area for both, they are stacked on top of each other
+          gridArea: "1/1",
+          position: "relative",
+          // This centers the other elements inside the hero component
+          placeItems: "center",
+          display: "grid",
+        }}>
+
+
+<Link to="https://www.amazon.co.jp/gp/product/B000J8V7WS/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B000J8V7WS&linkCode=as2&tag=dai5986-22&linkId=bc4768fc7ec145387bd0514ba74a31f9"><img border="0" src="//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=B000J8V7WS&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=dai5986-22" /></Link>
+<p><Link to="https://www.amazon.co.jp/gp/product/B000J8V7WS/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=B000J8V7WS&linkCode=as2&tag=dai5986-22&linkId=b883c931bcf6e376d9236ef3c46328bc">史的言語学における比較の方法</Link></p>
+        </div>
+
+
+<p>
+  言わずと知れた古典的名著。翻訳者でもあり京都大学の教授だった泉井久之助は原著を4冊買い（3冊ダメにしたということ）、死ぬ間際の枕元にも置いてあったといいます。
 </p>
 
 <hr />
